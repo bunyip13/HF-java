8328816c1f98959ad9ee7bf315c6dfa1ef12310d
Bastus <sebastian013@o2.pl>
2016-07-28 22:03:15 +0200

    change package

---
+++ src/pl/bastus/music/MyMusic.java
@@ -0,0 +1,51 @@
+package pl.bastus.music;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MyMusic {
+    private List<Song> songList = new ArrayList<>();
+
+    public static void main(String... args) {
+        MyMusic music = new MyMusic();
+        music.getWork();
+    }
+
+    private void getWork() {
+        getSongs();
+        System.out.print(songList);
+
+        Collections.sort(songList, (Song p1, Song p2) -> p1.getTitle().compareTo(p2.getTitle()));
+        Collections.sort(songList, (Song p1, Song p2) -> p1.getArtist().compareTo(p2.getArtist()));
+        Collections.sort(songList, (Song p1, Song p2) -> p1.getRate().compareTo(p2.getRate()));
+        Collections.sort(songList, (Song p1, Song p2) -> p1.getBpm().compareTo(p2.getBpm()));
+        songList.forEach(Song::printSong);
+    }
+
+    private void getSongs() {
+        try {
+            File file;
+            BufferedReader reader;
+            String line;
+
+            file = new File("y:\\Programowanie\\Tutorials\\ListaPiosenek.txt");
+            reader = new BufferedReader(new FileReader(file));
+            while ((line = reader.readLine()) != null) {
+                addSong(line);
+            }
+            reader.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void addSong(String l) {
+        String[] elements = l.split("/");
+        Song nextSong = new Song(elements[0], elements[1], elements[2], elements[3]);
+        songList.add(nextSong);
+    }
+}
